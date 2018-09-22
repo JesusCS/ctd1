dd96cc18aecdef43a117aaed8a8a169af88d0e07
Jesus Cabrera Santos <jesussantos0329@gmail.com>
2018-09-22 14:55:58 +0000

    add the puts

---
+++ week_one/reading_assignments/ruby_basics.rb
@@ -0,0 +1,21 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+
+# output the number of seconds in a week
+puts 1+1
+
+
+# output a float
+
+
+
+# output a string
+
+
+
+# output concatination of strings
