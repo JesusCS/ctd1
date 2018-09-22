ddd029ce5ef813c8ebaedd24f16a16b34975528f
Jesus Cabrera Santos <jesussantos0329@gmail.com>
2018-09-22 18:57:39 +0000

    testing binding

--- week_one/user_input_program/test_loop.rb
+++ week_one/user_input_program/test_loop.rb
@@ -1,12 +1,14 @@
 def main
+      # we need an array to store our data
+      # how do we assign an array to a variable?
     input = []
-  # we need an array to store our data
-  # how do we assign an array to a variable?
+    binding.pry
   loop do
+      # continue to ask for a number
       puts 'Enter a number:'
       value = gets.chomp
+      binding.pry
       input << value
-    # continue to ask for a number
     # challenge: how do I check that may input is an integer and not a string?
     break if value == '' # this will end the loop if the user input is blank
   end
